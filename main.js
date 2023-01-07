ffc2c755450093acbe24aaf629ba399b02c5c2fe
NahirB <nahirbts@outlook.com>
2023-01-07 15:21:14 -0300

    agrego la preentreg1

---
+++ main.js
@@ -0,0 +1,16 @@
+/** Algoritmos simples */
+
+let numero1 = parseInt(prompt("Ingrese número 1"));
+let numero2 = parseInt(prompt("Ingrese número 2"));
+
+// console.log(numero1);
+// numero1 = parseInt(numero1);
+// console.log(numero1);
+//
+// console.log(numero2);
+// numero2 = parseInt(numero2);
+// console.log(numero2);
+
+const resultadoSuma = numero1 + numero2;
+
+console.log("El resultado de la suma es: " + resultadoSuma);
