79e272848e3c6ffff37f2732aaa8c0e8290add21
NahirB <nahirbts@outlook.com>
2023-01-09 17:24:55 -0300

    agrego funciones

--- main.js
+++ main.js
@@ -1,49 +1,50 @@
 let cursoIngresado = prompt("Ingrese el nombre del curso que compró (Desarrollo, Idiomas o Diseño)")
 
-while (cursoIngresado !== "Desarrollo" && cursoIngresado !== "Idiomas" && cursoIngresado !== "Diseño") {
-
-    cursoIngresado(prompt("Ingrese la opción correcta (Desarrollo, Idiomas o Diseño)"));
+function consultarCurso(){
+    while (cursoIngresado !== "Desarrollo" && cursoIngresado !== "Idiomas" && cursoIngresado !== "Diseño") {
+        let cursoIngresado = (prompt("Ingrese la opción correcta (Desarrollo, Idiomas o Diseño)"));
+    }
+    mostrarInfoDeCursos(cursoIngresado)
 }
 
-switch(cursoIngresado){
+
+function mostrarInfoDeCursos(){
+
+    switch(cursoIngresado){
 
     case "Desarrollo":
             console.log("Su curso de " + cursoIngresado + " incia el dia 13/02/20223");
-            break
-
+            mostrarMenu()
+            break;
     case "Idiomas":
             console.log("Su curso de " + cursoIngresado + " incia el dia 20/02/20223");
-            break
-
+            mostrarMenu()
+            break;
     case "Diseño":
-            console.log("Su curso de " + cursoIngresado + " incia el dia 06/03/20223");
-            break
-
+            console.log("Su curso de " + cursoIngresado + " incia el dia 06/03/2023");
+            mostrarMenu()
+            break;
         default:
             console.log("Usted ingreso un curso que no se dicta")
+            mostrarInfoDeCursos()
             break;
+    }
 }
+mostrarInfoDeCursos()
 
-let otraConsulta = (prompt("Ingrese otro curso del que desee saber la fecha de inicio (Desarrollo, Idiomas o Diseño) o por el contrario Salir para terminar el proceso"));
-if (otraConsulta !== "Salir" && cursoIngresado !== "salir" && cursoIngresado !== "SALIR") {
+function mostrarMenu(){
+    let otraConsulta = (prompt("¿Desea realizar otra consulta? SI para continuar o NO parasalir del sistema)).toLowerCase();
 
-    switch(otraConsulta){
+ switch(otraConsulta){
 
-        case "Desarrollo":
-                console.log("Su curso de " + otraConsulta + " incia el dia 13/02/20223");
-                break
-    
-        case "Idiomas":
-                console.log("Su curso de " + otraConsulta + " incia el dia 20/02/20223");
-                break
-    
-        case "Diseño":
-                console.log("Su curso de " + otraConsulta + " incia el dia 06/03/20223");
-                break
-    
-            default:
-                console.log("Usted ingreso un curso que no se dicta")
+        case "si":
+            mostrarInfoDeCursos()
+                break;
+        case "no":
+                break;
+        default:
+                console.log("Error")
+                mostrarMenu()
                 break;
     }
-} else (otraConsulta === "Salir" && otraConsulta === "salir" && otraConsulta === "SALIR") 
-    alert ("Gracias por su consulta!");
+}
