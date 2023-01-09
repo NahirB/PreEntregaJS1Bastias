199cd96e4dec691c17118398522b15d8b8e4fa75
NahirB <nahirbts@outlook.com>
2023-01-09 10:57:48 -0300

    subo mi proyecto

--- main.js
+++ main.js
@@ -1,54 +1,25 @@
-/*Realizo un programa en el cual se ingrese el plan que quiere realizar la persona (A, B, C o D), la cantidad de horas que puede hacer por semana 
-y se calcule cuánto tiempo va a tardar, teniendo en cuenta que el curso dura 8 meses estudiando 5 hs por semana:
- * Estudiante del plan A = 2 hs por semana
- * Estuadiante del plan B = 3 hs por semana
- *  Estudiante del plan C = 4 hs por semana
- * Estudiante del plan D = 5 hs por semana
- */
+let cursoIngresado = prompt("Ingrese el nombre del curso que compró (Desarrollo, Idiomas o DIseño)")
 
-let estudiante = "";
+while (cursoIngresado !== "Desarrollo" && cursoIngresado !== "Idiomas" && cursoIngresado !== "DIseño") {
 
-while(estudiante !== "Salir") {
+    cursoIngresado(prompt("Ingrese la opción correcta (Desarrollo, Idiomas o Diseño)"));
+}
 
-    // Le pido al usuario el plam a realizar
-    let plan = prompt("Ingrese el curso (A, B, C o D)");
+switch(cursoIngresado){
 
-    // While 
-    while(plan !== "A" && plan !== "B" && plan !== "C" && plan !== "D") {
+    case "Desarrollo":
+            console.log("Su curso de " + cursoIngresado + " incia el dia 13/02/20223");
+            break
 
-        plan = prompt("Ingrese CORRECTAMENTE el plan (A, B, C o D)");
+    case "Idiomas":
+            console.log("Su curso de " + cursoIngresado + " incia el dia 20/02/20223");
+            break
 
-    }
+    case "Diseño":
+            console.log("Su curso de " + cursoIngresado + " incia el dia 06/03/20223");
+            break
 
-    // Le pido al estudiante las horas que puede estudiar por semana
-    const horasDeEstudio = parseInt(prompt("Ingrese la cantidad de horas que puede estudiar por semana"));
-
-    // Calcular cuanto le tomaria
-    let tiempo = 0;
-
-    switch(plan) {
-
-        case "A":
-            tiempo = (horasDeEstudio * 4) +  ;
+        default:
+            console.log("Usted ingreso in curso que no se dicta")
             break;
-
-        case "B":
-            tiempo = horasDeEstudio * 50;
-            break;
-
-        case "C":
-            tiempo = horasDeEstudio * 75;
-            break;
-
-        case "D":
-            tiempo = horasDeEstudio * 100;
-            break;
-    }
-
-    // Mostrar el resultado
-    console.log("El estudiante del plan " + plan + " por " + horasDeEstudio + " horas, le va a llevar: " + tiempo);
-
-    // Preguntar al usuario si quiere ingresar otro curso
-    curso = prompt("Ingrese Salir si no quiere ingresar otro curso");
-
 }
